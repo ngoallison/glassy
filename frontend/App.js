941c0dc3846a51bcf407f2fb1de87853dec67db4
Allison <allisontlngo@gmail.com>
2024-02-24 10:30:27 -0800

    remove info

---
+++ frontend/App.js
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { useFonts } from "expo-font";
+import { Text } from "react-native";
+
+import Front from "./pages/Front";
+import SignIn from "./pages/SignIn";
+import Register from "./pages/Register";
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    'BeVietnamProBold': require("./assets/fonts/BeVietnamPro-Bold.ttf"),
+    'BeVietnamProRegular': require("./assets/fonts/BeVietnamPro-Regular.ttf"),
+    'LibreCaslonTextBold': require("./assets/fonts/LibreCaslonText-Bold.ttf"),
+  });
+
+  if (!fontsLoaded) {
+    // Fonts are still loading, return a loading indicator or placeholder
+    return null;
+  }
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Front Page"
+        screenOptions={{ headerShown: false }}
+      >
+        <Stack.Screen name="Front Page" component={Front} />
+        <Stack.Screen name="Sign In Page" component={SignIn} />
+        <Stack.Screen name="Register Page" component={Register} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
