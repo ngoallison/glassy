cb8b37646064f6be39e0cbc71c7ebc0847a36244
Allison <allisontlngo@gmail.com>
2024-05-12 00:15:09 -0700

    Minimum Routine page

---
+++ frontend/components/LongImageCard.js
@@ -0,0 +1,31 @@
+import styles from "../styles";
+import { Text, View, Image, } from "react-native";
+import React from "react";
+import Ionicons from "react-native-vector-icons/Ionicons"
+
+const LongImageCard = (props) => {
+    return (
+        <View style={{ marginBottom: 15, height: 100, backgroundColor: "white", borderRadius: 20, flexDirection: "row", alignItems: "center", padding: 10 }}>
+            <View style={{ flex: 3, width: "100%", height: "100%", borderRadius: 15, backgroundColor: "gainsboro", justifyContent: "center", alignItems: "center" }}>
+                {props.item.photo ?
+                    <Image style={{ width: "100%", height: "100%", borderRadius: 15 }} source={{ uri: props.item.photo }} ></Image> :
+                    <Ionicons name="leaf-outline" color="gray" size={40}></Ionicons>
+                }
+            </View>
+
+            <View style={{ flex: 5, paddingLeft: 15, paddingRight: 15 }}>
+                <Text style={[styles.header, { textAlign: "left", fontSize: 14 }]}>{props.item.brand}</Text>
+                <Text style={[styles.lightText, { textAlign: "left", fontSize: 12 }]}>{props.item.name}</Text>
+            </View>
+            <View style={{ width: 1, height: "100%", backgroundColor: "#E2EBF4" }}></View>
+            <View style={{ flex: 2, justifyContent: "center", flexDirection: "row" }}>
+                <View style={{ gap: 15, alignItems: "center", justifyContent: "space-around" }}>
+                    <Ionicons name="sunny-outline" color="#3A405A" size={20}></Ionicons>
+                    <Text style={[styles.lightText, { fontSize: 10 }]}>{props.item.category}</Text>
+                </View>
+            </View>
+        </View>
+    );
+}
+
+export default LongImageCard;
--- frontend/pages/Routine/Routine.js
+++ frontend/pages/Routine/Routine.js
@@ -1,14 +1,77 @@
 import { StatusBar } from "expo-status-bar";
 import styles from "../../styles";
-import { Text, View } from "react-native";
-import React, { Component } from "react";
+import { Text, View, Pressable, Image, FlatList } from "react-native";
+import React, { Component, useState } from "react";
 import Header from "../Home/Header";
+import Ionicons from "react-native-vector-icons/Ionicons"
+import productsData from "../../assets/data/products.json";
+import LongImageCard from "../../components/LongImageCard";
+
 
 const Routine = ({ navigation }) => {
+    const days = ["S", "M", "T", "W", "T", "F", "S"]
+
+    const [selectedTime, setSelectedTime] = useState("Morning");
+    const [selectedDay, setSelectedDay] = useState(null); // State to track selected button
+
+    const handleButtonPress = (index) => {
+        if (index === selectedDay) {
+            // Deselect the button if it's already selected
+            setSelectedDay(null);
+        } else {
+            // Otherwise, select the button
+            setSelectedDay(index);
+        }
+    };
+
+    const handleTimePress = (name) => {
+        if (name == 'Morning') {
+            setSelectedTime("Morning");
+        }
+        else {
+            setSelectedTime("Night");
+        }
+    };
+
     return (
         <View style={styles.background}>
             <Header func={() => { navigation.openDrawer() }}></Header>
-            <Text>Hello World :D</Text>
+            <View style={{ flex: 1, padding: 20 }}>
+                <View style={{ flexDirection: "row", width: "100%", justifyContent: "space-between", gap: 10 }}>
+                    <Pressable
+                        id="Morning"
+                        onPress={() => handleTimePress("Morning")}
+                        style={[selectedTime == "Morning" ? styles.solidButton : styles.clearButton, { flex: 1, flexDirection: "row", gap: 10 }]}>
+                        <Ionicons size={20} color={selectedTime == "Morning" ? "#FFFFFF" : "#3A405A"} name="sunny-outline"></Ionicons>
+                        <Text style={[styles.boldText, { color: selectedTime == "Morning" ? "#FFFFFF" : "#3A405A" }]}>Morning</Text>
+                    </Pressable>
+                    <Pressable
+                        id="Night"
+                        onPress={() => handleTimePress("Night")}
+                        style={[selectedTime == "Night" ? styles.solidButton : styles.clearButton, { flex: 1, flexDirection: "row", gap: 10 }]}>
+                        <Ionicons size={20} color={selectedTime == "Night" ? "#FFFFFF" : "#3A405A"} name="moon-outline"></Ionicons>
+                        <Text style={[styles.boldText, { color: selectedTime == "Night" ? "#FFFFFF" : "#3A405A" }]}>Night</Text>
+                    </Pressable>
+                </View>
+                <View style={{ paddingTop: 20, flexDirection: "row", justifyContent: "space-between" }}>
+                    {days.map((day, index) => (
+                        <Pressable
+                            key={index}
+                            onPress={() => handleButtonPress(index)}
+                            style={{
+                                width: 40, height: 40, backgroundColor: selectedDay == index ? "#3A405A" : "#E2EBF4", borderRadius: 10, justifyContent: "center"
+                            }}>
+                            <Text style={[styles.boldText, { color: selectedDay == index ? "#FFFFFF" : "#3A405A" }]}>{day}</Text>
+                        </Pressable>
+                    ))}
+                </View>
+                <FlatList
+                    data={productsData}
+                    renderItem={({ item }) => <LongImageCard item={item}></LongImageCard>}
+                    style={{ marginTop: 20 }}
+                >
+                </FlatList>
+            </View>
         </View >
     );
 };
